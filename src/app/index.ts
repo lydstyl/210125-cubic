c19fa9a54e64c93dc25dbbebd52afb536db36c92
Gabriel Brun <lydstyl@gmail.com>
2021-01-26 20:44:34 +0100

    first tersts & launch.json for debug

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,27 @@
-export function sum(a: number, b: number): number {
-  return a + b
+export function toFixed4(num: number): number {
+  return parseFloat(num.toFixed(4))
 }
 
-console.log(sum(1, 2))
+export function area(length: number, width: number): number {
+  return length * width
+}
+
+export function cubicFloorArea(length: number): number {
+  return toFixed4(area(length, length))
+}
+
+export function cubicWallArea(length: number, height: number): number {
+  return length * height * 4
+}
+
+export function rate(a: number, b: number): number {
+  return toFixed4(a / b)
+}
+
+export function wallAreaRate(length: number): number {
+  const wallArea = cubicWallArea(length, 2.2)
+  const floorArea = cubicFloorArea(length)
+
+  const wallAreaRate = rate(wallArea, floorArea)
+  return toFixed4(wallAreaRate)
+}
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,7 +1,39 @@
-import { sum } from '../app/index'
+import { toFixed4, area, cubicFloorArea, cubicWallArea, rate, wallAreaRate } from '../app/index'
 
 describe('Main index test suite', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+  it('fix four number 0.3333', () => {
+    expect(toFixed4(0.33333)).toBe(0.3333)
+  })
+
+  it('fix four number 0.6667', () => {
+    expect(toFixed4(0.66666)).toBe(0.6667)
+  })
+
+  it('calculate area', () => {
+    expect(area(3, 4)).toBe(12)
+  })
+
+  it('calculate cubic floor area', () => {
+    expect(cubicFloorArea(3)).toBe(9)
+  })
+
+  it('calculate cubic floor area with 4 numbers after .', () => {
+    expect(cubicFloorArea(3.214550253)).toBe(10.3333)
+  })
+
+  it('calculate wall area', () => {
+    expect(cubicWallArea(3, 2)).toBe(24)
+  })
+
+  it('calculate rate with 4 numbers after . 0.66666666 --> 0.6667', () => {
+    expect(rate(2, 3)).toBe(0.6667)
+  })
+
+  it('calculate rate with 4 numbers after . 0.33333333 --> 0.3333', () => {
+    expect(rate(1, 3)).toBe(0.3333)
+  })
+
+  it('calculate wall area rate', () => {
+    expect(wallAreaRate(2)).toBe(4.4)
   })
 })
